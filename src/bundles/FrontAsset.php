dd84e93e172722f8b4cd9a3d37b6bb383cde8875
Clément Gayot <clement.g@lahautesociete.com>
2019-04-03 17:20:01 +0200

    :bug: Fix FrontAsset

--- src/bundles/FrontAsset.php
+++ src/bundles/FrontAsset.php
@@ -17,11 +17,6 @@ class FrontAsset extends AssetBundle
         // define the path that your publishable resources live
         $this->sourcePath = '@lhs/tarteaucitron/resources';
 
-        // define the dependencies
-        $this->depends = [
-            CpAsset::class,
-        ];
-
         // define the relative path to CSS/JS files that should be registered with the page
         // when this asset bundle is registered
         $this->js = [
